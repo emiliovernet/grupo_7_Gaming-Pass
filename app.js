730d4b8719373fd17f86ac7d1b64084bab0f2bbe
virginiaSchmied <118400773+virginiaSchmied@users.noreply.github.com>
2023-01-17 18:18:57 -0300

    Se actualiza app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -6,7 +7,10 @@ const mainRouter = require('./routers/main.js');
 
 app.use(express.static('public'));
 
-app.use('/', mainRouter)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use('/', mainRouter);
 
 app.listen(3030, () => {
     console.log('Servidor iniciado en http://localhost:3030');
