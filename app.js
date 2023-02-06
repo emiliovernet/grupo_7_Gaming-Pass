dd342d181ddcc30667198b7be915e57737639d95
AgusGranda <federico.granda@hotmail.com>
2023-02-06 20:58:00 -0300

    modifico app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const path = require('path');
+const methodOverride = require("method-override");
+
 
 const productsRouter = require('./routers/products');
 
-app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
+
+app.use(methodOverride("_method"))
+app.use(express.static(path.join(__dirname,"../public")));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
 
 app.use('/', productsRouter);
 
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
