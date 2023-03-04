2e5e4069149ccb633d94dcda4e3acdc5fe3de31f
AgusGranda <federico.granda@hotmail.com>
2023-03-04 17:07:36 -0300

    modifico Middleware

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -24,15 +24,7 @@ const controller = {
                 errors: resultadosDeValidacion.mapped(),
                 oldData: req.body
             });
-        }
-    
-        const nuevoUsuario = {
-                id: users[users.length - 1].id + 1,
-                ...req.body
-            };
-        users.push(nuevoUsuario);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
-        res.redirect('/')
+       }
     
     }
 
